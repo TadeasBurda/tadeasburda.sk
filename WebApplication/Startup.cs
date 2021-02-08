6ee8e153bd72227c0627342d4d645b1a5523bdbb
Tadeáš Burda <tadeas.burda@outlook.com>
2021-02-08 11:02:42 +0100

    I added Cache-Control for static files

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Net.Http.Headers;
 using WebApplication.Data;
 using WebApplication.Services;
 
@@ -51,7 +52,15 @@ namespace WebApplication
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    const int durationInSeconds = 60 * 60 * 24;
+                    ctx.Context.Response.Headers[HeaderNames.CacheControl] =
+                        "public,max-age=" + durationInSeconds;
+                }
+            });
 
             app.UseRouting();
 
